12cacde5fb6c6893b54260a08616440339f9be70
tihmihvik <tihmihvik@gmail.com>
2023-09-16 14:43:29 +0700

    2
    2

---
+++ app/src/main/java/ru/tihonov/cardandbox/MainActivity.kt
@@ -0,0 +1,95 @@
+package ru.tihonov.cardandbox
+
+import android.graphics.drawable.shapes.Shape
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.magnifier
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialogDefaults.shape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.tihonov.cardandbox.ui.theme.CardAndBoxTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+            ) {
+                ListItem("Miha", "Akter")
+                ListItem("Miha", "Akter")
+                ListItem("Miha", "Akter")
+                ListItem("Miha", "Akter")
+                ListItem("Miha", "Akter")
+                ListItem("Miha", "Akter")
+                ListItem("Miha", "Akter")
+                ListItem("Miha", "Akter")
+                ListItem("Miha", "Akter")
+                ListItem("Miha", "Akter")
+            }
+        }
+    }
+}
+
+@Composable
+private fun ListItem(name: String, prof: String){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp),
+        border = BorderStroke(1.dp, Color.Black)
+    ) {
+        Box {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "Картинка",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column(
+                    modifier = Modifier.padding(start = 16.dp)
+                ) {
+                    Text(text = name)
+                    Text(text = prof)
+                }
+            }
+        }
+    }
+}
